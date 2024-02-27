e919bc20aafd77ea3437619166b387df6731c03d
Jorge Correia <14-10254@usb.ve>
2024-02-26 21:28:41 -0400

    maybe listo

--- Tarea_5/pregunta_3.py
+++ Tarea_5/pregunta_3.py
@@ -37,9 +37,14 @@ class Node:
             n = n+1
             val = n
             suc = Node(parent=self,board=board,value=val,pos=i)
-            successors.append(suc)
-        return successors
+            yield suc
+         #   successors.append(suc)
+        #return successors
 
+    def hasSuccessors(self,player):
+        if (len(self.availablePos(player)) > 0):
+            return True
+        return False 
 
     def availablePos(self,player):
         available = []
@@ -53,7 +58,7 @@ class Node:
         return available
 
     def checkWinner(self):
-        print(f"Verificando la posicion {self.modPosition}")
+        #print(f"Verificando la posicion {self.modPosition}")
         position = self.modPosition
         rowNum = floor(position/3)
         colNum = position % 3
@@ -64,7 +69,7 @@ class Node:
             if i == "+":
                 count+= 1
         if count == 3:
-            print(f"La fila ganadora: {row}")
+            #print(f"La fila ganadora: {row}")
             return True
         count = 0
         col = []
@@ -74,7 +79,7 @@ class Node:
             if i == "+":
                 count+= 1
         if count == 3:
-            print(f"La columna ganadora: {col}")
+            #print(f"La columna ganadora: {col}")
             return True
         if self.board[0] == "+" and self.board[4] == "+" and self.board[8] == "+":
             return True
@@ -82,29 +87,25 @@ class Node:
             return True
         return False
     
-    def evaluate(self,symbol):
+    def evaluate(self,player):
         if (self.checkWinner()):
-            if symbol == "|":
+            if player == Primer_Jugador:
                 return -1
-            else:
+            elif player == Segundo_Jugador:
                 return 1
-
         else:
             return 0
         
 def primero(nodo,depth, alfa, beta):
-    succ = nodo.successors(Primer_Jugador)
-    if depth == 0 or len(succ) == 0:
-        nodo.getBoard()
-        print(nodo)
-        return nodo.evaluate(symbol)
+    
+    if depth == 0 or not nodo.hasSuccessors(Primer_Jugador):
+        #nodo.getBoard()
+        #print(nodo)
+        return nodo.evaluate(Primer_Jugador)
 
     else:
         mejor = -inf
-        if symbol == "-":
-                symbol = "|"    
-        else:
-            symbol = "-"
+        succ = nodo.successors(Primer_Jugador)
         for i in succ:               
             mejor = max(mejor,segundo(i,depth-1,alfa,beta))
             alfa = max(alfa,mejor)
@@ -113,105 +114,21 @@ def primero(nodo,depth, alfa, beta):
         return mejor
 
 def segundo(nodo,depth,alfa,beta):
-    succ = nodo.successors(Segundo_Jugador)
-    if depth == 0 or len(succ) == 0:
-        nodo.getBoard()
-        print(nodo)
-        return nodo.checkWinner()
+    
+    if depth == 0 or not nodo.hasSuccessors(Segundo_Jugador):
+        #nodo.getBoard()
+        #print(nodo)
+        return nodo.evaluate(Segundo_Jugador)
     
     else:
         mejor = inf
-        if symbol == "-":
-            symbol = "|"
-        else:
-            symbol = "-"
+        succ = nodo.successors(Segundo_Jugador)
         for i in succ:
-            mejor = min(mejor,primero(i,depth-1,alfa,beta,symbol))
+            mejor = min(mejor,primero(i,depth-1,alfa,beta))
             beta = min(beta,mejor)
             if beta <= alfa:
                 break
         return mejor
-
-
-""" class LaVieja:
-    def __init__(self):
-        self.board = [" "," "," "," "," "," "," "," "," "]
-
-    def doMovement(self,position,symbol):
-        if self.board[position] == " ":
-            self.board[position] = symbol
-            if self.checkWinner(position,symbol):
-                return True
-        elif self.board[position] == "-" and symbol == "|":
-            self.board[position] = "+"
-            if self.checkWinner(position,symbol):
-                return True
-        elif self.board[position] == "|" and symbol == "-":
-            self.board[position] = "+"
-            if self.checkWinner(position,symbol):
-                return True
-        self.printBoard()
-        return False
-        
-    
-    def checkWinner(self,position,symbol):
-        rowNum = floor(position/3)
-        colNum = position % 3
-        row = self.board[rowNum*3:(rowNum+1)*3]
-        # Check row
-        count = 0
-        for i in row:
-            if i == "+":
-                count+= 1
-        if count == 3:
-            print(f"La fila ganadora: {row}")
-            print(f"Gano {symbol} 0")
-            return True
-        count = 0
-        col = []
-        for i in range(0,3):
-            col.append(self.board[colNum+i*3])
-        for i in col:
-            if i == "+":
-                count+= 1
-        if count == 3:
-            print(f"La columna ganadora: {col}")
-            print(f"Gano {symbol} 1")
-            return True
-        if self.board[0] == "+" and self.board[4] == "+" and self.board[8] == "+":
-            print(f"Gano {symbol} 2")
-            return True
-        elif self.board[2] == "+" and self.board[4] == "+" and self.board[6] == "+":
-            print(f"Gano {symbol} 3")
-            return True
-        return False
-
-    
-    def availableStates(self,symbol):
-        available = []
-        for x in enumerate(self.board):
-            if symbol == "-":
-                if x[1] == " " or x[1] == "|":
-                    available.append(x[0])
-            if symbol == "|":
-                if x[1] == " " or x[1] == "-":
-                    available.append(x[0])
-        print(f"Disponibles para el {symbol}: {available}")
-        return available
-    
-    def emptyPos(self):
-        return ' ' in self.board
-       
-    def printBoard(self):
-        for i in range(0,9,3):
-            print(f" {self.board[i]} | {self.board[i+1]} | {self.board[i+2]}")
-            if i == 0 or i == 3:
-                print("-----------")
-        print("===========")
-
-    def getMoves(self,symbol):
-        return random.choice(self.availableStates(symbol)) """
-    
 
 # Un tablero es un arreglo de largo 9 con 4 posibles valores, +, -, | o " " (espacio en blanco).
 
@@ -222,30 +139,9 @@ def main():
     raiz = Node(None,base,0,None)
     alfa = -inf
     beta = inf
-    depth = 20
+    depth = 100
 
-    """ suc = raiz.successors("-")
-    for i in suc:
-        print("================")
-        print(i)
-        i.getBoard()
-    suc_1 = suc[0].successors("|")
-    for j in suc_1:
-        print("================")
-        print(j)
-        j.getBoard()
-    suc_1_2 = suc_1[0].successors("-")
-    for j in suc_1_2:
-        print("================")
-        print(j)
-        j.getBoard()
-    suc_1_2_3 = suc_1_2[0].successors("|")
-    for j in suc_1_2_3:
-        print("================")
-        print(j)
-    
-            j.getBoard()  """
-    print(primero(raiz,depth,alfa,beta))
+    print(segundo(raiz,depth,alfa,beta))
 
 
     
